fc2e7aa9f15003c9d90705ce5117f7513219cac1
zulfianasyafa <zulfianaauliasyafa@gmail.com>
2020-10-16 07:48:14 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/mytestingapp/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.dicoding.mytestingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.StringBuilder
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var btnSetValue : Button
+    private lateinit var tvText : TextView
+
+    private var names = ArrayList<String>()
+    private lateinit var imgPreview: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSetValue=findViewById(R.id.btn_set_value)
+        tvText = findViewById(R.id.tv_text)
+        btnSetValue.setOnClickListener(this)
+
+        names.add("Zulfiana Aulia Syafa")
+        names.add("Stella")
+        names.add("Shafiqah")
+
+        imgPreview  = findViewById(R.id.image_preview)
+        imgPreview.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.fronalpstock_big))
+        Glide.with(this).load(R.drawable.fronalpstock_big).into(imgPreview)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_set_value) {
+            Log.d("MainActivity", names.toString())
+            val name = StringBuilder()
+            for (i in 0..2) {
+                name.append(names[i]).append("\n")
+            }
+            tvText.text = name.toString()
+        }
+    }
+}
